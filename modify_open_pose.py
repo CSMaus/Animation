67931f7cb34421343a9e25ce4030b90e1d56c191
Kseniia Barashok <cs-maus-1@mail.ru>
2024-06-11 17:16:13 +0900

    original works. just do not create the names of folders with "_"

--- modify_open_pose.py
+++ modify_open_pose.py
@@ -2,21 +2,24 @@ import os
 import json
 
 input_dir = 'D:/ML_DL_AI_stuff/!playing_stuff/openpose/output_json_folder3/'
-output_dir = 'D:/ML_DL_AI_stuff/!playing_stuff/openpose/output_json_folder3_modified/'
+output_dir = 'D:/ML_DL_AI_stuff/!playing_stuff/openpose/keypointstest/'
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-for filename in os.listdir(input_dir):
-    if filename.endswith('.json'):
-        with open(os.path.join(input_dir, filename), 'r') as f:
-            data = json.load(f)
+json_files = [f for f in os.listdir(input_dir) if f.endswith('.json')]
 
-        for person in data['people']:
-            if 'person_id' in person:
-                del person['person_id']
+for index, filename in enumerate(sorted(json_files)):
+    with open(os.path.join(input_dir, filename), 'r') as f:
+        data = json.load(f)
 
-        with open(os.path.join(output_dir, filename), 'w') as f:
-            json.dump(data, f)
+    data['version'] = 1.2  # Change the JSON version to 1.2
+    for person in data['people']:
+        if 'person_id' in person:
+            del person['person_id']
+
+    new_filename = f"frame_{index:04d}_keypoints.json"
+    with open(os.path.join(output_dir, new_filename), 'w') as f:
+        json.dump(data, f)
 
 print("Post-processing completed. JSON files saved to", output_dir)
